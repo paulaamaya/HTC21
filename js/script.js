86a221c4b0d544dac7d493f7fff0259685c217ba
Jacob A <jacobatzori@hotmail.com>
2021-02-19 21:50:19 -0500

    added years to select & add family member button functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-jQuery(document).ready(function(){
-    $("#deleteme").html('Hello!');
-    $("#change-button").click(function(){
-        $("#deleteme").html('clicked!');
-    })
+$(document).ready(function(){
+    for (let i = 0; i < 121; i++){
+        let currentYear = 2020;
+        $('#yearSelect').append(new Option(""+(currentYear-i), ""+(currentYear-i)));
+    }
+    var familyMemberCount = 0;
+    $('#addFamilyMemberButton').click(function(){
+        familyMemberCount++;
+        let label = $("<label>").text(`Family Member ${familyMemberCount}`);
+        let input = $('<input type="text" class="form-control" placeholder="Health Card Number">').attr({name: `member${familyMemberCount}`});
+        let wrapper = $('<div class="family-member-card">');
+
+        wrapper.append(label);
+        wrapper.append(input);
+        $('#familyMemberCards').append(wrapper);
+    });
 });
